3aef1b8d168142ddb10e35c3c07d5d9fda24405a
ChristopherMeki <christopher.meki@omnicontact.biz>
2025-09-05 11:22:30 +0200

    init

--- src/main/java/com/example/backend/repository/FixtureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Fixture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FixtureRepository extends JpaRepository<Fixture, Long> {
-}
